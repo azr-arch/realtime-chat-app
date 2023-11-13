e72624495b4b3ae49a2fbd07d035b00733bab333
Azar <79351619+azr-arch@users.noreply.github.com>
2023-11-13 19:42:25 +0530

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["axios", "mongoose"],
+    },
     images: {
         domains: ["th.bing.com", "res.cloudinary.com"],
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            bufferutil: "commonjs bufferutil",
+            "supports-color": "commonjs supports-color",
+        });
+        return config;
     },
 };
 
