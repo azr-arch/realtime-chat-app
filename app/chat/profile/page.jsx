72e515d19fa8c87ce6777febc03076245f753ca8
Azar <79351619+azr-arch@users.noreply.github.com>
2023-11-26 12:53:11 +0530

    fix rendering of profile-page

--- app/chat/profile/page.jsx
+++ app/chat/profile/page.jsx
@@ -1,13 +1,22 @@
 import ProfilePage from "@components/profile-page";
-import { useUserServer } from "@hooks/use-user";
+import { connectToDB } from "@lib/db";
+import User from "@models/user";
+import { getServerSession } from "next-auth";
 import { Suspense } from "react";
 
 const Page = async () => {
-    const data = await useUserServer();
+    const db = await connectToDB();
+    const data = await getServerSession();
+
+    if (!data?.user) return null;
+
+    const user = await User.findOne({ email: data.user.email }).select("_id name email avatar");
+
+    if (!user) return null;
     return (
         <div className="w-full h-full self-stretch flex flex-col items-center justify-center">
             <Suspense fallback={<p>Loading...</p>}>
-                <ProfilePage data={data?.user} />
+                <ProfilePage data={user.toObject()} />
             </Suspense>
         </div>
     );
